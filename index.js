1515d038ca22f2b7cd9e239b1be358d0eafb0482
Thomas Walsh <46968416+sharpnachos@users.noreply.github.com>
2022-08-31 17:44:01 -0500

    HTML Form completed

---
+++ index.js
@@ -0,0 +1,9 @@
+function outputData(){
+    var x = document.getElementById("frm1");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length ;i++) {
+        text += x.elements[i].value + "<br>";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
